575bf8ce6227fcc75ca2a546c369dc69e34e2070
MPE <mpe2010manoj@gmail.com>
2016-12-07 22:10:27 +0530

    Added Polymorphism concept

--- system/Helpers/PurchaseOrder/Basket.php
+++ system/Helpers/PurchaseOrder/Basket.php
@@ -2,86 +2,37 @@
 
 namespace Pos\Helpers\PurchaseOrder;
 
-use Pos\Helpers\Cart\Storage;
-use Pos\Helpers\Database;
+use Pos\Helpers\Cart\Basket as Cart;
 
-class Basket {
+use stdClass;
 
-    private $storage;
-    private $db;
+class Basket extends Cart{
 
-    public function __construct(Storage $storage, Database $db) {
-        $this->storage = $storage;
-        $this->db = $db;
-    }
-
-    public function add($pid, $quantity) {
-        if ($this->has($pid)) {
+    public function add(stdClass $product, $quantity) {
+        if ($this->has($product)) {
             $quantity = $quantity;
         }
-        $this->update($pid, $quantity);
+        $this->update($product, $quantity);
     }
 
-    public function update($pid, $quantity) {
-        $this->storage->add($pid, [
-            'id' => (int) $pid,
+    public function update(stdClass $product, $quantity) {
+        $this->storage->add($product->id, [
+            'product_id' => (int) $product->id,
             'quantity' => (int) $quantity
         ]);
     }
 
-    public function all() {
-        $ids = [];
-        $items = [];
-        foreach ($this->storage->all() as $product) {
-            $ids[] = $product['id'];
-        }
-        $products = $this->db->batch('product', $ids);
-
-        foreach ($products as $product) {
-            //convert to std class because the product obj comes as readbeean php obj
-            $product = $this->db->convert($product);
-
-           $product->order_quantity = $this->get($product->id)['quantity'];
-           $items[] = $product;
-        }
-        return $items;
-    }
-
-    public function get($pid) {
-        return $this->storage->get($pid);
-    }
-
-    public function has($pid) {
-        return $this->storage->exists($pid);
-    }
-
-    public function remove($pid) {
-        $this->storage->delete($pid);
-    }
-
-    public function clear() {
-        $this->storage->clear();
-    }
-    
     public function total() {
         $total = 0;
-
         foreach ($this->all() as $item) {
-
             $product = $this->db->get('product', 'id = ?', [$item->id])->first();
-
             $total = $total + $item->price * $item->order_quantity;
         }
         return $total;
     }
 
-    public function count() {
-        return count($this->storage);
-    }
-
     public function refresh() {
         foreach ($this->all() as $item) {
-
             $product = $this->db->get('product', 'id = ?', [$item->id])->first();
             if(!$product) {
                 $this->remove($item->id);
